b486b5196d0109e0d85eb48a2a4d81ccc91f1d1c
geesunny <jiseonyang08@gmail.com>
2025-07-10 10:14:10 +0900

    ✨feat: 로그인 폼 기본 구조 구현

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,59 @@
-// src/app/login/page.tsx
+'use client';
+
+import { useState } from 'react';
+import Link from 'next/link';
+import Input from '@/components/common/Input';
 
 const LoginPage = () => {
-    return <div className="p-8 text-center">로그인 페이지</div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    alert('API 연결 전입니다ㅎㅎ');
+  };
+
+  return (
+    <main>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <Link href='/'>
+            <img src='/icons/logoHorizon.svg' alt='GlobalNomad Logo' />
+          </Link>
+        </div>
+
+        {/* 이메일 입력 */}
+        <Input
+          label='이메일'
+          type='email'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        {/* 비밀번호 입력 */}
+        <Input
+          label='비밀번호'
+          type='password'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        {/* 로그인 버튼 */}
+        <button type='submit'>로그인하기</button>
+
+        {/* 카카오 로그인 */}
+        <button>
+          <img src='/icons/icon_kakao.svg' alt='kakaoicon' />
+          카카오 로그인
+        </button>
+
+        {/* 회원가입 링크 */}
+        <p>
+          회원이 아니신가요? <Link href='/signup'>회원가입하기</Link>
+        </p>
+      </form>
+    </main>
+  );
 };
 
 export default LoginPage;
