f94ba4821f669a3596571522d6d647211af9a26a
Jo Insung <leeming01@naver.com>
2025-07-10 17:20:29 +0900

    ✨feat: 페이지 적용

--- src/app/activities/[id]/page.tsx
+++ src/app/activities/[id]/page.tsx
@@ -1,10 +1,17 @@
 // app/activities/[id]/page.tsx
 import PhotoSection from '@/components/activities/PhotoSection';
+import ReviewCard from '@/components/activities/ReviewCard';
 const ActivityPage = async ({ params }: { params: { id: string } }) => {
   return (
     <>
       <div>Activity ID: {params.id}</div>
       <PhotoSection />
+      <ReviewCard
+        nickname='아무개'
+        rating={3}
+        content='저는 저희 스트릿 댄서 체험에 참가하게 된 지 얼마 안됐지만, 정말 즐거운 시간을 보냈습니다. 새로운 스타일과 춤추기를 좋아하는 나에게 정말 적합한 체험이었고, 전문가가 직접 강사로 참여하기 때문에 어떤 수준의 춤추는 사람도 쉽게 이해할 수 있었습니다. 강사님께서 정말 친절하게 설명해주셔서 정말 좋았고, 이번 체험을 거쳐 새로운 스타일과 춤추기에 대한 열정이 더욱 생겼습니다. 저는 이 체험을 적극 추천합니다!"'
+        createdAt='2025-07-10T06:45:09.546Z'
+      />
     </>
   );
 };
