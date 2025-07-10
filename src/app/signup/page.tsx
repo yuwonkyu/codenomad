ee5f6a3ac6a1aed7c09d0ed104df89ad74f695bd
geesunny <jiseonyang08@gmail.com>
2025-07-10 11:31:39 +0900

    ✨feat: 회원가입 페이지 기본 레이아웃 및 구조 생성

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,7 +1,34 @@
-// src/app/signup/page.tsx
+'use client';
+
+import { useState } from 'react';
+import Link from 'next/link';
 
 const SignupPage = () => {
-    return <div className="p-8 text-center">회원가입 페이지</div>;
+  const [email, setEmail] = useState('');
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+  };
+
+  return (
+    <main className='min-h-screen flex justify-center px-1 pt-60 lg:pt-100'>
+      <form
+        onSubmit={handleSubmit}
+        className='w-full max-w-376 md:max-w-640 space-y-24 p-24 md:p-32 bg-white rounded-16'
+      >
+        {/* 로고 */}
+        <div>
+          <Link href='/'>
+            <img
+              src='/icons/logoHorizon.svg'
+              alt='GlobalNomad Logo'
+              className='mx-auto mb-32 w-144 md:w-255'
+            />
+          </Link>
+        </div>
+      </form>
+    </main>
+  );
 };
 
 export default SignupPage;
