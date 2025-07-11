9dbedbc3fbbd1d078463a902dca3e67d1bc76b02
geesunny <jiseonyang08@gmail.com>
2025-07-11 15:54:18 +0900

    ✨feat: 회원가입 함수 signupApi 추가

--- src/lib/api/auth.ts
+++ src/lib/api/auth.ts
@@ -1,5 +1,6 @@
 // lib/api/auth.ts
 // loginApi 함수 → 로그인 요청을 보내고, 성공 시 accessToken, refreshToken, user 정보를 반환하도록
+// signupApi 함수 → 회원가입 요청을 보내고, 성공 시 사용자 정보를 반환
 import instance from './axios';
 
 interface LoginPayload {
@@ -7,7 +8,18 @@ interface LoginPayload {
   password: string;
 }
 
+interface SignupPayload {
+  email: string;
+  password: string;
+  nickname: string;
+}
+
 export const loginApi = async ({ email, password }: LoginPayload) => {
   const response = await instance.post('/auth/login', { email, password });
   return response.data; // accessToken, refreshToken, user 포함됨
+};
+
+export const signupApi = async (data: SignupPayload) => {
+  const response = await instance.post('/15-5/users', data);
+  return response.data;
 };
