88ad6febdc32499529940e6bbce031191e3a550f
geesunny <jiseonyang08@gmail.com>
2025-07-11 15:03:03 +0900

    ✨feat: axios instance 설정 파일 생성

--- src/lib/api/axios.ts
+++ src/lib/api/axios.ts
@@ -1,1 +1,12 @@
 //Axios 인스턴스
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: 'https://sp-globalnomad-api.vercel.app/15-5/',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  withCredentials: true, // 쿠키 인증 시 필요
+});
+
+export default instance;
