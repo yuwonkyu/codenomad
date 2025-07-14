99ca0d2e54f38e6cf2b628b5bcdd38bb232e9dbd
geesunny <jiseonyang08@gmail.com>
2025-07-14 17:30:09 +0900

    🎨style: searchBar 스타일 수정

--- src/components/landing/SearchBar.tsx
+++ src/components/landing/SearchBar.tsx
@@ -18,7 +18,7 @@ const SearchBar = () => {
   };
 
   return (
-    <section className='w-full mt-32 md:mt-50 flex flex-col items-center gap-4 whitespace-nowrap'>
+    <section className='w-full mt-32 md:mt-50 flex flex-col items-center gap-4'>
       <h2 className='text-16-b md:text-32-b text-gray-950'>무엇을 체험하고 싶으신가요?</h2>
 
       <div className='relative w-full max-w-[1080px]'>
@@ -30,7 +30,10 @@ const SearchBar = () => {
         {/* 검색 버튼 */}
         <button
           onClick={handleSearch}
-          className='absolute right-10 top-1/2 -translate-y-1/2 w-100 h-50 md:h-36 bg-primary-500 text-white text-14-b md:text-16-b rounded-[12px] px-4 hover:bg-blue-400 transition cursor-pointer'
+          className='absolute right-4 top-1/2 -translate-y-1/2 
+                     w-[85px] h-[41px] md:w-[120px] md:h-[50px] 
+                     bg-primary-500 text-white text-14-b md:text-16-b 
+                     rounded-[14px] hover:bg-blue-400 transition cursor-pointer'
         >
           검색하기
         </button>
@@ -42,7 +45,9 @@ const SearchBar = () => {
           onChange={(e) => setKeyword(e.target.value)}
           onKeyDown={handleKeyDown}
           placeholder='내가 원하는 체험은?'
-          className='w-full h-53 rounded-[12px] pl-30 pr-32 text-gray-500 text-14-m md:h-70 text-base focus:outline-none cursor-pointer'
+          className='w-full h-53 rounded-[12px] 
+                     pl-[30px] pr-[95px] md:pr-[130px] 
+                     text-gray-500 text-14-m md:h-70 focus:outline-none cursor-pointer'
           style={{
             boxShadow: '0px 6px 10px rgba(13, 153, 255, 0.05)',
           }}
