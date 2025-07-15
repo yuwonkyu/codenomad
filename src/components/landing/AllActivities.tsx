7484128018ac97537190e094ba580ee15bfd2569
geesunny <jiseonyang08@gmail.com>
2025-07-15 15:30:42 +0900

    ✨feat: 페이지 네이션 공통 컴포넌트 구현& 홈메인 적용

--- src/components/landing/AllActivities.tsx
+++ src/components/landing/AllActivities.tsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import CategoryFilter from './CategoryFilter';
 import PriceSortDropdown from './PriceSortDropdown';
 import LandingCard from './LandingCard';
+import Pagination from '../common/Pagination';
 
 const mockActivities = [
   {
@@ -75,6 +76,7 @@ const mockActivities = [
 const AllActivities = () => {
   const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
   const [selectedSort, setSelectedSort] = useState<string | null>(null);
+  const [currentPage, setCurrentPage] = useState(1);
 
   return (
     <section className='mt-80 mb-160'>
@@ -83,7 +85,6 @@ const AllActivities = () => {
         <h2 className='text-20-b md:text-24-b'>🛼 모든 체험</h2>
         <PriceSortDropdown selectedSort={selectedSort} onSelectSort={setSelectedSort} />
       </div>
-
       {/* 카테고리 필터 (아래로 내려오기) */}
       <div className='mb-20'>
         <CategoryFilter
@@ -91,13 +92,18 @@ const AllActivities = () => {
           onSelectCategory={setSelectedCategory}
         />
       </div>
-
       {/* 카드 목록 */}
       <div className='grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 gap-16 sm:gap-24 no-scrollbar'>
         {mockActivities.map((item) => (
           <LandingCard key={item.id} {...item} />
         ))}
       </div>
+      {/* 페이지네이션 */}
+      <Pagination
+        currentPage={currentPage}
+        totalPages={7}
+        onPageChange={(page) => setCurrentPage(page)}
+      />
     </section>
   );
 };
