c7b305cac8337ad45294cb2d752d74dd0e8fd954
geesunny <jiseonyang08@gmail.com>
2025-07-17 14:16:59 +0900

    🗑️remove: 인기체험 api연동전 Mockdata 제거

--- src/components/landing/AllActivities.tsx
+++ src/components/landing/AllActivities.tsx
@@ -6,72 +6,7 @@ import PriceSortDropdown from './PriceSortDropdown';
 import LandingCard from './LandingCard';
 import Pagination from '../common/Pagination';
 
-const mockActivities = [
-  {
-    id: 1,
-    bannerImageUrl: '/imgs/banner.jpg',
-    title: '요가 클래스',
-    rating: 4.9,
-    reviewCount: 120,
-    price: 15000,
-  },
-  {
-    id: 2,
-    bannerImageUrl: '/imgs/thumbnail.jpg',
-    title: '수제 도자기 만들기',
-    rating: 4.8,
-    reviewCount: 95,
-    price: 20000,
-  },
-  {
-    id: 3,
-    bannerImageUrl: '/imgs/thumbnail2.jpg',
-    title: '스트릿 댄스 워크샵',
-    rating: 4.7,
-    reviewCount: 87,
-    price: 18000,
-  },
-  {
-    id: 4,
-    bannerImageUrl: '/imgs/banner.jpg',
-    title: '로컬 농장 체험 프로그램',
-    rating: 4.9,
-    reviewCount: 120,
-    price: 19000,
-  },
-  {
-    id: 5,
-    bannerImageUrl: '/imgs/thumbnail.jpg',
-    title: '해녀 일일 체험',
-    rating: 4.8,
-    reviewCount: 95,
-    price: 30000,
-  },
-  {
-    id: 6,
-    bannerImageUrl: '/imgs/banner.jpg',
-    title: '챌린지 파크 액티비티 체험',
-    rating: 4.7,
-    reviewCount: 87,
-    price: 40000,
-  },
-  {
-    id: 7,
-    bannerImageUrl: '/imgs/thumbnail.jpg',
-    title: '수제 도자기 만들기',
-    rating: 4.8,
-    reviewCount: 95,
-    price: 20000,
-  },
-  {
-    id: 8,
-    bannerImageUrl: '/imgs/thumbnail2.jpg',
-    title: '스트릿 댄스 워크샵',
-    rating: 4.7,
-    reviewCount: 87,
-    price: 18000,
-  },
-];
+
 
 const AllActivities = () => {
   const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
