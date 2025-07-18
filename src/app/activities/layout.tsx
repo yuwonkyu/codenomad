f222eaa87a2a1ccafa37a38716f3d0ed7380edd6
Joinsung <leeming01@naver.com>
2025-07-18 14:51:35 +0900

    🗑️remove: Script 제거

--- src/app/activities/layout.tsx
+++ src/app/activities/layout.tsx
@@ -1,5 +1,4 @@
 import { ReactNode } from 'react';
-import Script from 'next/script';
 
 export default function ActivitiesLayout({ children }: { children: ReactNode }) {
   return (
@@ -7,11 +6,6 @@ export default function ActivitiesLayout({ children }: { children: ReactNode }) 
       <div className='mx-24 my-30 flex flex-col gap-20 md:mx-30 md:my-34 md:gap-30 lg:mx-12 lg:my-88 lg:items-center'>
         {children}
       </div>
-      <Script
-        type='text/javascript'
-        src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_API_KEY}&libraries=services,clusterer&autoload=false`}
-        strategy='beforeInteractive'
-      />
     </>
   );
 }
