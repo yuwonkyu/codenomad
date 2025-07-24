48aabde28af44e7a72109e44880228f46f5b5076
sonsujin <sonsj33@gmail.com>
2025-07-24 15:54:33 +0900

    ✨feat: zustand store, store 타입 정의

---
+++ src/store/useAuthStore.ts
@@ -0,0 +1,20 @@
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
+import { AuthState, UserType } from './useAuthStore.types';
+
+export const useAuthStore = create<AuthState>()(
+  persist(
+    (set) => ({
+      accessToken: null,
+      refreshToken: null,
+      user: null,
+      isLoggedIn: false,
+      setAccessToken: (token) => set({ accessToken: token, isLoggedIn: true }),
+      setRefreshToken: (token: string) => set({ refreshToken: token }),
+      clearAuthStore: () =>
+        set({ accessToken: null, refreshToken: null, user: null, isLoggedIn: false }),
+      setUser: (user: UserType) => set({ user: user }),
+    }),
+    { name: 'auth-storage' },
+  ),
+);
