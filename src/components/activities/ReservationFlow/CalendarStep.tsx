fbaba0824b54539aa6867ceef020b32d75a8d00c
Joinsung <leeming01@naver.com>
2025-07-25 02:11:55 +0900

    ✨feat: 예약 가능 날짜만 선택되도록 조건문 수정 및 isDateDisabled, onClickDay 핸들러 적용

--- src/components/activities/ReservationFlow/CalendarStep.tsx
+++ src/components/activities/ReservationFlow/CalendarStep.tsx
@@ -1,6 +1,7 @@
 import Calendar from '@/components/common/Calendar';
 import { Schedule } from '../Activities.types';
 import { useMemo } from 'react';
+
 interface CalendarStepProps {
   schedules: Schedule[];
   selectedDate: string | null;
@@ -8,12 +9,9 @@ interface CalendarStepProps {
 }
 
 const CalendarStep = ({ schedules, selectedDate, onDateSelect }: CalendarStepProps) => {
-  // UTC 명시적 지정으로 날짜 밀림 방지
-  const stringToDate = (dateStr: string): Date => {
-    return new Date(dateStr + 'T12:00:00.000Z'); // UTC 정오로 변환
-  };
+  // 날짜 변환 함수
+  const stringToDate = (dateStr: string): Date => new Date(dateStr + 'T12:00:00.000Z');
 
-  // Date → String 변환 (로컬 시간 기준으로 UTC 밀림 방지)
   const dateToString = (date: Date): string => {
     const year = date.getFullYear();
     const month = String(date.getMonth() + 1).padStart(2, '0'); // getMonth()는 0부터 시작하므로 +1
@@ -21,24 +19,25 @@ const CalendarStep = ({ schedules, selectedDate, onDateSelect }: CalendarStepPro
     return `${year}-${month}-${day}`;
   };
 
+  // 예약 가능한 날짜 Set
   const availableDates = useMemo(() => new Set(schedules.map((s) => s.date)), [schedules]);
-  console.log(availableDates);
 
+  // 날짜 변경(예약 가능 날짜만 허용)
   const handleDateChange = (date: Date | null) => {
     if (date) {
       const dateStr = dateToString(date);
-      onDateSelect(dateStr);
+      if (availableDates.has(dateStr)) {
+        onDateSelect(dateStr);
+      }
     }
   };
 
   return (
-    <>
-      {/* 기존 캘린더에서 사용할 수 있는 속성이 누락 되어 있어서 누락된 부분 적용 X */}
-      <Calendar
-        selectedDate={selectedDate ? stringToDate(selectedDate) : null}
-        onChange={handleDateChange}
-      />
-    </>
+    <Calendar
+      selectedDate={selectedDate ? stringToDate(selectedDate) : null}
+      onClickDay={handleDateChange}
+      isDateDisabled={(date) => !availableDates.has(dateToString(date))}
+    />
   );
 };
 
