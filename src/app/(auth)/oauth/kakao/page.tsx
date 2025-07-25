3bd437b3464916167cac6b76211b6f43e7afdb35
geesunny <jiseonyang08@gmail.com>
2025-07-25 17:17:45 +0900

    ✨feat: kakao oauth signin 간편로그인 로직 추가

---
+++ src/app/(auth)/oauth/kakao/page.tsx
@@ -0,0 +1,47 @@
+'use client';
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import instance from '@/lib/api/axios';
+import { useAuthStore } from '@/store/useAuthStore';
+
+const KakaoCallbackPage = () => {
+  const router = useRouter();
+  const setAccessToken = useAuthStore((state) => state.setAccessToken);
+  const setRefreshToken = useAuthStore((state) => state.setRefreshToken);
+
+  const setUser = useAuthStore((state) => state.setUser);
+
+  useEffect(() => {
+    const fetchKakaoToken = async () => {
+      const code = new URL(window.location.href).searchParams.get('code');
+      if (!code) return;
+
+      try {
+        const loginRes = await instance.post('oauth/sign-in/kakao', {
+          token: code,
+          redirectUri: process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI,
+        });
+
+        const { accessToken, refreshToken, user } = loginRes.data;
+        setAccessToken(accessToken);
+        setRefreshToken(refreshToken);
+
+        setUser(user);
+
+        router.replace('/');
+      } catch (err: any) {
+        if (err?.response?.status === 404) {
+          router.replace(`/oauth/kakao/signup?code=${code}`);
+        } else {
+          alert('카카오 로그인 실패');
+        }
+      }
+    };
+
+    fetchKakaoToken();
+  }, [router, setAccessToken, setRefreshToken, setUser]);
+
+  return <div>카카오 로그인 처리 중...</div>;
+};
+
+export default KakaoCallbackPage;
