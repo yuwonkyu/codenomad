5539f6d3f69e59559f818376dcadf07bd41689dc
Joinsung <leeming01@naver.com>
2025-07-26 16:16:42 +0900

    ✨feat: 클릭 이벤트 처리 추가 - 기존 onClick과 onClickImage로 이벤트 구별

--- src/components/common/SafeImage.tsx
+++ src/components/common/SafeImage.tsx
@@ -6,12 +6,30 @@ import { useState } from 'react';
 interface SafeImageProps extends Omit<ImageProps, 'src'> {
   src: string;
   fallbackSrc?: string;
+  onClickImage?: (src: string) => void;
 }
 
-const SafeImage = ({ src, fallbackSrc = '/icons/empty.svg', alt, ...props }: SafeImageProps) => {
+const SafeImage = ({
+  src,
+  fallbackSrc = '/icons/empty.svg',
+  alt,
+  onClickImage,
+  ...props
+}: SafeImageProps) => {
   const [imgSrc, setImgSrc] = useState(src);
 
-  return <Image {...props} src={imgSrc} alt={alt} onError={() => setImgSrc(fallbackSrc!)} />;
+  return (
+    <Image
+      {...props}
+      onClick={(e) => {
+        props.onClick?.(e); // 기본 onClick
+        onClickImage?.(imgSrc); // 선택적으로 imgSrc 넘기는 기능
+      }}
+      src={imgSrc}
+      alt={alt}
+      onError={() => setImgSrc(fallbackSrc!)}
+    />
+  );
 };
 
 export default SafeImage;
