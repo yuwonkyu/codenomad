1492be5afd032ea1080c3c190ae8cab1cf34b4ae
Joinsung <leeming01@naver.com>
2025-07-26 16:49:32 +0900

    🫧modify: errorAlt 조건문 수정

--- src/components/common/SafeImage.tsx
+++ src/components/common/SafeImage.tsx
@@ -20,14 +20,14 @@ const SafeImage = ({
 }: SafeImageProps) => {
   const [error, setError] = useState(false);
   const imageSrc = error ? fallbackSrc : src;
-  const errorAlt = error ? alt : '이미지를 불러오는데 실패하였습니다.';
+  const errorAlt = error ? '이미지를 불러오는데 실패하였습니다.' : alt;
 
   return (
     <Image
       {...props}
       onClick={(e) => {
         props.onClick?.(e); // 기본 onClick
-        onClickImage?.({ src: imageSrc, alt: errorAlt }); // 선택적으로 imgSrc 넘기는 기능
+        onClickImage?.({ src: imageSrc, alt: errorAlt }); // 선택적으로 imageSrc 넘기는 기능
       }}
       src={imageSrc}
       alt={errorAlt}
