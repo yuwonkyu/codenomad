7a76ce30f017450cb9ac56e61cf67644e7eaadf7
YuWonKyu <duck01777@naver.com>
2025-07-29 20:11:28 +0900

    ✨ TitleInput 컴포넌트에 react-hook-form 통합: onChange 핸들러 제거 및 props 수정

--- src/components/myExperiencesAddEdit/TitleInput.tsx
+++ src/components/myExperiencesAddEdit/TitleInput.tsx
@@ -1,33 +1,25 @@
+import { UseFormRegister, FieldValues, Path } from 'react-hook-form';
 import Input from '@/components/common/Input';
 
-interface TitleInputProps {
+interface TitleInputProps<T extends FieldValues> {
+  register: UseFormRegister<T>;
+  error?: string;
   value: string;
-  onChange: (value: string) => void;
 }
 
-const TitleInput = ({ value, onChange }: TitleInputProps) => {
-  const handleChange = (
-    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>,
-  ) => {
-    const inputValue = e.target.value;
-    if (inputValue.length <= 30) {
-      onChange(inputValue);
-    }
-  };
-
-  return (
-    <div className='mb-24'>
-      <Input
-        label='제목'
-        labelClassName='text-16-b'
-        placeholder='제목을 입력해 주세요'
-        value={value}
-        onChange={handleChange}
-        maxLength={30}
-      />
-      <div className='text-12-m text-right text-gray-400'>{value.length}/30</div>
-    </div>
-  );
-};
+const TitleInput = <T extends FieldValues>({ register, error, value }: TitleInputProps<T>) => (
+  <div className='mb-24'>
+    <Input
+      label='제목'
+      labelClassName='text-16-b'
+      placeholder='제목을 입력해 주세요'
+      maxLength={30}
+      {...register('title as Path<T>)}')}
+      error={error}
+      value={value}
+    />
+    <div className='text-12-m text-right text-gray-400'>{value.length}/30</div>
+  </div>
+);
 
 export default TitleInput;
