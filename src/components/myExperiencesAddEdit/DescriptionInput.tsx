c10be0ef51b517d223515cc3b0143a6ed1d13984
YuWonKyu <duck01777@naver.com>
2025-07-29 20:11:05 +0900

    ✨ DescriptionInput 컴포넌트에 react-hook-form 통합: onChange 핸들러 제거 및 props 수정

--- src/components/myExperiencesAddEdit/DescriptionInput.tsx
+++ src/components/myExperiencesAddEdit/DescriptionInput.tsx
@@ -1,36 +1,31 @@
 import Input from '@/components/common/Input';
+import { UseFormRegister, FieldValues, Path } from 'react-hook-form';
 
-interface DescriptionInputProps {
+interface DescriptionInputProps<T extends FieldValues> {
+  register: UseFormRegister<T>;
+  error?: string;
   value: string;
-  onChange: (value: string) => void;
 }
 
-// 화살표 함수식 및 타입 호환성 문제 해결
-const DescriptionInput = ({ value, onChange }: DescriptionInputProps) => {
-  const handleChange = (
-    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>,
-  ) => {
-    const inputValue = e.target.value;
-    if (inputValue.length <= 1000) {
-      onChange(inputValue);
-    }
-  };
-
-  return (
-    <div className='mb-24'>
-      <Input
-        label='설명'
-        labelClassName='text-16-b'
-        as='textarea'
-        placeholder='체험에 대한 설명을 입력해 주세요'
-        value={value}
-        onChange={handleChange}
-        className='min-h-108 md:min-h-168'
-        maxLength={1000}
-      />
-      <div className='text-12-m text-right text-gray-400'>{value.length}/1000</div>
-    </div>
-  );
-};
+const DescriptionInput = <T extends FieldValues>({
+  register,
+  error,
+  value,
+}: DescriptionInputProps<T>) => (
+  <div className='mb-24'>
+    <Input
+      label='설명'
+      labelClassName='text-16-b'
+      as='textarea'
+      placeholder='체험에 대한 설명을 입력해 주세요'
+      maxLength={1000}
+      {...register('description' as Path<T>)}
+      error={error}
+      value={value}
+      className='min-h-108 md:min-h-168'
+    />
+    <div className='text-12-m text-right text-gray-400'>{value.length}/1000</div>
+  </div>
+);
 
 export default DescriptionInput;
