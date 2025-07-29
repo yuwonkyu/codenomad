91dd294cf55aec53b4a8d5a594d11f1ecff3b5f3
YuWonKyu <duck01777@naver.com>
2025-07-29 20:35:50 +0900

    ✨ AddressInput, TitleInput, PriceInput, DescriptionInput, CategoryInput, BannerImageInput, IntroImagesInput, ReserveTimesInput 타입 정의 추가

---
+++ src/components/myExperiencesAddEdit/types.ts
@@ -0,0 +1,68 @@
+import { FieldValues, UseFormRegister } from 'react-hook-form';
+
+// AddressInput
+export interface AddressInputProps<T extends FieldValues> {
+  register: UseFormRegister<T>;
+  error?: string;
+  value: string;
+  detailAddress?: string;
+  onDetailAddressChange?: (value: string) => void;
+  detailError?: string;
+}
+
+// TitleInput
+export interface TitleInputProps<T extends FieldValues> {
+  register: UseFormRegister<T>;
+  error?: string;
+  value: string;
+}
+
+// PriceInput
+export interface PriceInputProps<T extends FieldValues> {
+  register: UseFormRegister<T>;
+  error?: string;
+  value: string;
+}
+
+// DescriptionInput
+export interface DescriptionInputProps<T extends FieldValues = FieldValues> {
+  register: UseFormRegister<T>;
+  error?: string;
+  value: string;
+}
+
+// CategoryInput
+export interface CategoryInputProps {
+  value: string;
+  onChange: (value: string) => void;
+  options: { value: string; label: string }[];
+  error?: string;
+}
+
+// BannerImageInput
+export interface BannerImageInputProps {
+  bannerPreview: string | null;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onRemove: () => void;
+  banner: File | null;
+  isEdit?: boolean;
+}
+
+// IntroImagesInput
+export interface IntroImagesInputProps {
+  introPreviews: string[];
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onRemove: (idx: number) => void;
+}
+
+// ReserveTimesInput
+export interface ReserveTime {
+  date: string;
+  start: string;
+  end: string;
+}
+export interface ReserveTimesInputProps {
+  value: ReserveTime[];
+  onChange: (value: ReserveTime[]) => void;
+  isEdit?: boolean;
+}
