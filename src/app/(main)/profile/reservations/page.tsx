43dfaa96ee0e015a48ec698b489f42b4878d7315
sonsujin <sonsj33@gmail.com>
2025-07-31 01:09:31 +0900

    ✨feat: 뒤로가기 버튼 추가

--- src/app/(main)/profile/reservations/page.tsx
+++ src/app/(main)/profile/reservations/page.tsx
@@ -1,11 +1,13 @@
 'use client';
+import MobilePageHeader from '@/components/profile/common/MobilePageHeader';
 import Badge from '@/components/reservationList/Badge';
 import ReservationCard from '@/components/reservationList/ReservationCard';
 import { StatusType } from '@/components/reservationList/StatusBadge';
 import { getReservationList } from '@/lib/api/profile/reservationList';
 import Image from 'next/image';
 import Link from 'next/link';
-import { useEffect, useState, useRef, useCallback } from 'react';
+import { useEffect, useState, useRef, useCallback, useContext } from 'react';
+import { ProfileMobileContext } from '../layout';
 
 interface reservationsType {
   activity: {
@@ -35,7 +37,7 @@ const Page = () => {
   const [cursorId, setCursorId] = useState<number | null>(null);
   const [isLoading, setIsLoading] = useState(false);
   const [hasMore, setHasMore] = useState(true);
-
+  const mobileContext = useContext(ProfileMobileContext);
   const observerEl = useRef<HTMLDivElement>(null);
   const statusList: { text: string; value: StatusType }[] = [
     { text: '예약 신청', value: 'pending' },
@@ -118,7 +120,10 @@ const Page = () => {
 
   return (
     <div className='mx-auto flex w-full flex-col justify-center p-24 lg:px-126'>
-      <h1 className='text-18-b text-gray-950'>예약 내역</h1>
+      <h1 className='text-18-b text-gray-950'>
+        <MobilePageHeader title='예약 내역' />
+        예약 내역
+      </h1>
       <h2 className='text-14-m my-10 text-gray-500'>예약 내역을 변경 및 취소 할 수 있습니다.</h2>
 
       <div className='scrollbar-hide overflow-x-scroll'>
