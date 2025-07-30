781952dfe61fef870f16b1a90092f67f8f477e3c
Joinsung <leeming01@naver.com>
2025-07-31 03:59:43 +0900

    🫧modify: 컨벤션에 맞게 수정

--- src/components/activities/sections/ActivityHeader.tsx
+++ src/components/activities/sections/ActivityHeader.tsx
@@ -9,24 +9,26 @@ interface ActivityHeaderProps {
   activityId: number;
 }
 
-const ActivityHeader = ({ activity, isOwner, activityId }: ActivityHeaderProps) => (
-  <header className='flex justify-between border-b-1 border-gray-100 pb-12'>
-    <div className='flex flex-col'>
-      <p className='text-13-m mb-4 text-gray-700'>{activity.category}</p>
-      <h1 className='text-18-b mb-16 text-gray-950'>{activity.title}</h1>
-      <div className='mb-10 flex gap-0.5'>
-        <Image src='/icons/icon_star_on.svg' alt='별' width={16} height={16} />
-        <p className='text-14-m text-gray-700'>
-          {activity.rating}({activity.reviewCount})
-        </p>
+const ActivityHeader = ({ activity, isOwner, activityId }: ActivityHeaderProps) => {
+  return (
+    <header className='flex justify-between border-b-1 border-gray-100 pb-12'>
+      <div className='flex flex-col'>
+        <p className='text-13-m mb-4 text-gray-700'>{activity.category}</p>
+        <h1 className='text-18-b mb-16 text-gray-950'>{activity.title}</h1>
+        <div className='mb-10 flex gap-0.5'>
+          <Image src='/icons/icon_star_on.svg' alt='별' width={16} height={16} />
+          <p className='text-14-m text-gray-700'>
+            {activity.rating}({activity.reviewCount})
+          </p>
+        </div>
+        <div className='flex gap-0.5'>
+          <Image src='/icons/icon_map.svg' alt='지도 마크' width={16} height={16} />
+          <p className='text-14-m text-gray-700'>{activity.address}</p>
+        </div>
       </div>
-      <div className='flex gap-0.5'>
-        <Image src='/icons/icon_map.svg' alt='지도 마크' width={16} height={16} />
-        <p className='text-14-m text-gray-700'>{activity.address}</p>
-      </div>
-    </div>
-    {isOwner && <DropdownMenu activityId={activityId} />}
-  </header>
-);
+      {isOwner && <DropdownMenu activityId={activityId} />}
+    </header>
+  );
+};
 
 export default ActivityHeader;
