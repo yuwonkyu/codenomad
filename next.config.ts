8f7ec1dab173127d8382e6e6cf0c13df213c4543
YuWonKyu <duck01777@naver.com>
2025-07-31 12:02:46 +0900

    🐛fix: eslint 설정 추가하여 빌드 중 무시하도록 변경

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['sprint-fe-project.s3.ap-northeast-2.amazonaws.com'],
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
